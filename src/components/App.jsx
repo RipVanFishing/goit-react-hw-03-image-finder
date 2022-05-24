280fcaee01201d70bb7c5adb668e72bf5a95b291
Denis Serkhel <ripvanfish27@gmail.com>
2022-05-24 15:54:13 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
 import SearchBar from './SearchBar';
 import ImageGallery from './ImageGallery';
 import Button from './Button';
+import { Component } from 'react';
 
 import styles from './Styles.module.css'
+import { ToastContainer } from 'react-toastify';
 
-export const App = () => {
-  return (
-    <div className={styles.app}>
-      <SearchBar />
-      <ImageGallery />
-      <Button/>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    text: "",
+  }
+  handleSubmitData = text => {
+    
+    this.setState({ text });
+  } 
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <SearchBar onSubmit={this.handleSubmitData} />
+        <ImageGallery />
+        <Button />
+        <ToastContainer autoClose={3000} position='top-right'/>
+      </div>
+    );
+  }
 };
+
+export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,21 +1,50 @@
 import { Component } from "react";
+import { toast } from "react-toastify";
 import css from './SearchBar.module.css';
 
- class SearchBar extends Component {
+class SearchBar extends Component {
+     
+    state = {
+        text: '',
+    }
+
+    handleSearch = (e) => {
+        e.preventDefault();
+        if (this.state.text.trim() === '') {
+            return toast.info("Введите предмет поиска :D")
+        }
+        this.props.onSubmit(this.state.text)
+
+        this.reset();
+
+    }
+    handleChange = (e) => {
+        const { name, value } = e.currentTarget;
+        this.setState({[name]:value})
+    }
+    reset = () => {
+        this.setState({
+            text: '',
+        })
+    }
+    
     render() {
         return(
         <header className={css.searchbar}>
-  <form className={css.searchForm}>
+  <form onSubmit={this.handleSearch} className={css.searchForm}>
     <button type="submit" className={css.searchForm_button}>
       <span className={css.searchForm_button_label}>Search</span>
     </button>
 
     <input
-      className={css.searchForm_input}
-      type="text"
-      autocomplete="off"
-      autofocus
-      placeholder="Search images and photos"
+    className={css.searchForm_input}
+     value={this.state.text}
+    onChange={this.handleChange}
+    type="text"
+    autoComplete="off"
+ autoFocus
+                        placeholder="Search images and photos"
+                        name="text"
     />
   </form>
     </header>
